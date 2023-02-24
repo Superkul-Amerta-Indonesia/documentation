0f92851809b9c0dd70cd151fa163f435115e62a7
arif rahman fauzi <arif.rahman1780@gmail.com>
2023-02-24 15:56:58 +0700

    update logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,10 +64,10 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Superkul Documentation',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: 'Superkul Logo',
+          src: 'img/logo-superkul-icon.svg',
         },
         items: [
           {
